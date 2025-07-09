4275ca42f9a1d671640fc2e067c78cec37e2c995
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-07-09 21:48:35 +0300

    setup completed

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+#function to get all requirements and return them as a List
+def get_requirements()->List:
+    requirements_list: List[str] = []
+    try:
+        with open('requirements.txt', 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                requirement = line.strip()
+                if requirement and requirement != '-e .':
+                    requirements_list.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found")
+
+    return requirements_list
+print(get_requirements())
+
+setup(
+    name = 'doctors_appointment_booking',
+    version = '0.0.1',
+    author = "jimmy muthoni",
+    author_email = "jimmymuthoni26@gmail.com",
+    packages = find_packages(),
+    install_requires = get_requirements()
+)
+
