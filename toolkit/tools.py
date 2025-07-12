40ab83e4283b034075033155a5560d8886590681
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-07-12 16:36:57 +0300

    agents tools completed

--- toolkit/tools.py
+++ toolkit/tools.py
@@ -91,7 +91,21 @@ def cancel_appointment(date:DateTimeModel, id_number:IdentificationNumberModel, 
       return "Successfully cancelled."
 
 
-
+# Function to reschedule appointment
+@tool
+def reschedule_appointment(old_date: DateTimeModel, new_date:DateTimeModel, id_number: IdentificationNumberModel, doctor_name:Literal['kevin anderson','robert martinez','susan davis','daniel miller','sarah wilson','michael green','lisa brown','jane smith','emily johnson','john doe']):
+    """
+    Rescheduling an appointment.
+    The parameters MUST be provided by the user
+    """
+    df = pd.read_csv("/home/brian/Documents/JIM/Doctor Appointment Multiagent/data/doctor_availability.csv")
+    available_for_desired_date = df[(df['date_slot'] == new_date.date)&(df['is_available'] == True)&(df['doctor_name'] == doctor_name)]
+    if len(available_for_desired_date) == 0:
+        return "No available slot in the desired period"
+    else:
+        cancel_appointment.invoke({'date':old_date, 'id_number':id_number, 'doctor_name':doctor_name})
+        set_appointment.invoke({'desired_date':new_date, 'id_number': id_number, 'doctor_name': doctor_name})
+        return "Successfully rescheduled for the desired time."
 
 
 
