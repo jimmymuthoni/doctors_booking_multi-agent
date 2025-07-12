597cc786f9f7c6821881e048bdabe1d4167293cd
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-07-12 16:29:31 +0300

    tool to cancel appointment

--- toolkit/tools.py
+++ toolkit/tools.py
@@ -73,6 +73,24 @@ def set_appointment(desired_date:DateTimeModel, id_number:IdentificationNumberMo
 
         return "Successfully done"
 
+#function to cancel the appointmnet
+@tool
+def cancel_appointment(date:DateTimeModel, id_number:IdentificationNumberModel, doctor_name: Literal['kevin anderson','robert martinez','susan davis','daniel miller','sarah wilson','michael green','lisa brown','jane smith','emily johnson','john doe']):
+    """
+    Cancelling doctor appointment.
+    The paramaters should be provided by the user in the query
+    """
+    df = pd.read_csv("/home/brian/Documents/JIM/Doctor Appointment Multiagent/data/doctor_availability.csv")
+    case_to_remove = df[(df['date_slot'] == date.date)&(df['patient_to_attend'] == id_number.id)&(df['doctor_name'] == doctor_name)]
+    if len(case_to_remove) == 0:
+        return "You don't have any appointment with that specification."
+    else:
+      df.loc[(df['date_slot'] == date.date) & (df['patient_to_attend'] == id_number.id) & (df['doctor_name'] == doctor_name), ['is_available', 'patient_to_attend']] = [True, None]
+      df.to_csv(f'availability.csv', index = False)
+
+      return "Successfully cancelled."
+
+
 
 
 
