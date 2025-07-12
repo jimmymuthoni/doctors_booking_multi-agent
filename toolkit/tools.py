7ab89dfd8bd2917d777f7d02c4bf8a643d4f2df0
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-07-12 16:11:45 +0300

    availabiliy by specialization module

--- toolkit/tools.py
+++ toolkit/tools.py
@@ -11,8 +11,7 @@ def check_availability_by_doctor(desired_date: Datemodel, doctor_name: Literal['
     Checking the database if we have availability for the specific doctor.
     The parameters shouls be mentioned by the user in the query.
     """
-    df = pd.read_csv("data/doctor_availability.csv")
-
+    df = pd.read_csv("/home/brian/Documents/JIM/Doctor Appointment Multiagent/data/doctor_availability.csv")
     df['date_slot_time'] = df['date_slot'].apply(lambda input: input.split(' ')[-1])
     rows = list(df[(df['date_slot'].apply(lambda input: input.split(' ')[0]) == desired_date.date)&(df['doctor_name'] == doctor_name)&(df['is_available'] == True)]['date_slot_time'])
 
@@ -24,4 +23,38 @@ def check_availability_by_doctor(desired_date: Datemodel, doctor_name: Literal['
     return output
 
 
+#function to check availabilty by specialiality
+@tool
+def check_availability_by_specialization(desired_date: Datemodel, specialization: Literal["general_dentist", "cosmetic_dentist", "prosthodontist", "pediatric_dentist","emergency_dentist","oral_surgeon","orthodontist"]):
+    """
+    checking the database if we have the availability for the specific specialization.
+    The prameters should be mentioned by user in the query
+    """
+    df = pd.read_csv("/home/brian/Documents/JIM/Doctor Appointment Multiagent/data/doctor_availability.csv")
+    df['date_slot_time'] = df['date_slot'].apply(lambda input: input.split(' ')[-1])
+    rows = df[(df['date_slot'].apply(lambda input: input.split(' ')[0]) == desired_date.date) & (df['specialization'] == specialization) & (df['is_available'] == True)].groupby(['specialization', 'doctor_name'])['date_slot_time'].apply(list).reset_index(name='available_slots')
 
+    if len(rows) == 0:
+        output = "No availabiliy in the entire day"
+    else:
+        def convert_to_am_pm(time_str):
+            #split time string into hour and minutes
+            time_str = str(time_str)
+            hours, minutes = map(int, time_str.split(":"))
+            period = 'AM' if hours < 12 else 'PM'
+            #conveting hours to 12 -hour format
+            hours = hours % 12 or 12
+            return f"{hours}: {minutes:02d} {period}"
+        output = f"This availability for {desired_date.date}\n"
+        for row in rows.values:
+            output += row[1] + ". Available slots: \n" + ', \n'.join([convert_to_am_pm(value) for value in row[2]])+'\n'
+
+    return output
+
+
+
+
+
+
+
+    
