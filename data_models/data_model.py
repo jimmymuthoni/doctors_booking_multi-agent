14b47e5b7c9e29c8eebe0145a7f01e2c75374db8
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-07-15 14:07:05 +0300

    added input field verification

--- data_models/data_model.py
+++ data_models/data_model.py
@@ -3,28 +3,29 @@ from pydantic import BaseModel, Field, field_validator
 
 #class to verify datetime imputs from the user
 class DateTimeModel(BaseModel):
-    date: str = Field(description = "properly formated date", pattern = r'^\d{2}-\d{2}-\d{4} \d{2}:\d{2}$')
-    @field_validator
-    def check_date_format(cls, v):
+    date: str = Field(description="properly formated date", pattern=r'^\d{2}-\d{2}-\d{4} \d{2}:\d{2}$')
+    @field_validator("date")
+    def check_date_fromat(cls, v):
         if not re.match(r'^\d{2}-\d{2}-\d{4} \d{2}:\d{2}$', v):
-            raise ValueError ("The date should be in format: 'DD-MM-YYYY HH:MM'")
+            raise ValueError("The date should be in format 'DD-MM-YYYY HH:MM'")
         return v
     
 #class to verfy date
-class Datemodel(BaseModel):
-    date: str = Field(description = "Properly formatted date", pattern = r'^\d{2}-\d{2}-\d{4}$')
-    @field_validator
+class DateModel(BaseModel):
+    date: str = Field(description="properly formated date", pattern=r'^\d{2}-\d{2}-\d{4}$')
+    @field_validator("date")
+    
     def check_date_format(cls, v):
         if not re.match(r'^\d{2}-\d{2}-\d{4}$', v):
-            raise ValueError("The date must be in the format:'DD-MM-YYYY'")
+            raise ValueError("The date must be in the format: 'DD-MM-YYYY'")
         return v
     
 #class for verfying patient id
 class IdentificationNumberModel(BaseModel):
-    id: int = Field(description="Identification number 7 or 8 digits long")
-    @field_validator
-    def check_id_format(cls, v):
-        if not re.match(r'^\d{7,8}$', str(v)): #convering strings before matching
+    id: int = Field(description="Identification number (7 or 8 digits long)")
+    @field_validator("id")
+    def check_format_id(cls, v):
+        if not re.match(r'^\d{7,8}$', str(v)):  # Convert to string before matching
             raise ValueError("The ID number should be a 7 or 8-digit number")
         return v
          
